56761108ceb7658460b3e1bb9b3b9d607b54e82a
Miroslav Hadzhiev <miroslav.hadzhiev@gmail.com>
2020-01-18 20:23:06 +0200

    Upgrade to kINd v0.7.0. `prerequisites-cmds.sh` Improvements.

--- local-cluster/create-one-node.sh
+++ local-cluster/create-one-node.sh
@@ -5,10 +5,6 @@ set -e
 kind create cluster --config ./kind-config-one.yaml --name kind-one
 # Deploy Tiller
 kubectl create -f ../tiller.yaml
-# Deploy "local-path" hostPath provisioner
-kubectl create -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false", "storageclass.beta.kubernetes.io/is-default-class":"false"}}}'
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true", "storageclass.beta.kubernetes.io/is-default-class":"true"}}}'
 # Deploy MetalLB
 kubectl create -f https://raw.githubusercontent.com/google/metallb/master/manifests/metallb.yaml
 kubectl create -f ../metallb-config.yaml
@@ -24,7 +20,6 @@ do
 done
 # Init Helm Client only
 helm init --client-only
-export KUBECONFIG="$(kind get kubeconfig-path --name="kind-one")"
 # Deploy NGINX Ingress Controller for local K8s
 helm install --name ingress stable/nginx-ingress --set controller.extraArgs.enable-ssl-passthrough="",controller.hostNetwork=true,controller.kind=DaemonSet
 # Put Node Labels
--- local-cluster/prerequisites-cmds.sh
+++ local-cluster/prerequisites-cmds.sh
@@ -13,35 +13,36 @@ else
     exit 1
 fi
 
-# Start Docker
-systemctl start docker
+# Unmask and start Docker service
+sudo systemctl unmask docker \
+&& sudo systemctl start docker
 
 # Install "kubectl"
-echo -e "\nDownloading kubectl binary...\n" \
+echo -e "\nDownloading kubectl binary..." \
 && curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl \
 && chmod +x ./kubectl \
 && sudo mv ./kubectl /usr/local/bin/kubectl \
-&& echo -e "kubectl version:\n" \
+&& echo -e "\nkubectl version:" \
 && kubectl version 2>/dev/null \
 && source <(kubectl completion bash)
 
 # Install "helm"
 HELM_VERSION=v2.16.1 \
-&& echo -e "\nDownloading Helm Client binary...\n" \
+&& echo -e "\nDownloading Helm Client binary..." \
 && curl -LO https://get.helm.sh/helm-"$HELM_VERSION"-linux-amd64.tar.gz \
 && tar xf helm-"$HELM_VERSION"-linux-amd64.tar.gz \
 && mv ./linux-amd64/helm ./linux-amd64/tiller /usr/local/bin \
 && rm -rf ./linux-amd64 helm-"$HELM_VERSION"-linux-amd64.tar.gz \
-&& echo -e "helm version:\n" \
+&& echo -e "\nhelm version:" \
 && helm version 2>/dev/null \
 && source <(helm completion bash)
 
 # Install kINd
-KIND_VERSION=v0.6.1 \
-&& echo -e "\nDownloading kINd binary...\n" \
+KIND_VERSION=v0.7.0 \
+&& echo -e "\nDownloading kINd binary..." \
 && curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/"$KIND_VERSION"/kind-$(uname)-amd64 \
 && chmod +x ./kind \
 && mv ./kind /usr/local/bin/kind \
-&& echo -e "kINd version:\n" \
+&& echo -e "\nkINd version:" \
 && kind version \
 && source <(kind completion bash)
