41e7325f0b242a8cf8a772fa830d3c900d581fe0
Miroslav Hadzhiev <miroslav.hadzhiev@gmail.com>
2020-02-09 12:50:10 +0200

    DRY further. Up to 9 nodes.

--- local-cluster/create-nodes.sh
+++ local-cluster/create-nodes.sh
@@ -5,22 +5,19 @@ NO_NODES="${1}"
 
 case "${NO_NODES}" in
   "1")
-  CLUSTER_WRKS=(kind-1-control-plane)
+  CLUSTER_WRKS=(kind-"${NO_NODES}"-control-plane)
   ;;
-  "2")
-  CLUSTER_WRKS=(kind-2-worker2)
-  ;;
-  "3")
-  CLUSTER_WRKS=(kind-3-worker2 kind-3-worker3)
+  "[2-9]")
+  CLUSTER_WRKS=(kind-"${NO_NODES}"-worker2)
   ;;
   *)
-  echo -e "\e[32m\n\e[1mPass the number of the desired nodes - 1|2|3.\e[00m"
+  echo -e "\e[32m\n\e[1mPass the number of the desired nodes - 1|2|3|4|5|6|7|8|9.\e[00m"
   exit 11
   ;;
 esac
 
 # Create kINd cluster
-kind create cluster --config ./kind-config-"${NO_NODES}".yaml --name kind-"${NO_NODES}"
+kind create cluster --config ./kind-config.yaml --name kind-"${NO_NODES}"
 # Deploy MetalLB
 kubectl create -f https://raw.githubusercontent.com/google/metallb/master/manifests/metallb.yaml
 kubectl create -f ../metallb-config.yaml
