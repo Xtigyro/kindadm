f8f65f353728c6acf3c10df9e4680a32d40dc79a
Xtigyro <miroslav.hadzhiev@gmail.com>
2020-06-11 22:52:10 +0300

    [helm-v2] configurable k8s and helm versions

--- local-cluster/prerequisites-cmds.sh
+++ local-cluster/prerequisites-cmds.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+# default Helm version
+HELM_VER='2.16.8'
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --helm_ver=*|-hv=*)
+      if [[ "$1" != *=* ]]; then shift; fi
+      if [[ "$1" != *=2.*.* ]]; then
+        printf "\nIncompatible Helm ver.\nSupported syntax/version: ${LIGHT_GREEN}3.[x].[x]${NC}\n"
+        exit 1
+      fi
+      HELM_VER="${1#*=}"
+      ;;
+    --help|-h)
+      printf "\nUsage:\n    ${LIGHT_GREEN}--helm_ver,-hv${NC}      Set Helm version to be deployed.\n    ${LIGHT_GREEN}--help,-h${NC}           Prints this message.\nExample:\n    ${LIGHT_GREEN}bash $0 -hv=3.2.3${NC}\n" # Flag argument
+      exit 0
+      ;;
+    *)
+      >&2 printf "\nError: ${LIGHT_GREEN}Invalid argument${NC}\n"
+      exit 2
+      ;;
+  esac
+  shift
+done
+
 # Install req. pkgs
 OS_ID=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
 
@@ -10,7 +38,7 @@ elif [ "$OS_ID" == "ubuntu" ] ; then
     apt update && apt install -y docker.io curl
 else
     echo "Use "${0}" only on RHEL / CentOS / Ubuntu "
-    exit 1
+    exit 3
 fi
 
 
@@ -29,12 +57,11 @@ echo -e "\nDownloading kubectl binary..." \
 && source <(kubectl completion bash 2>/dev/null)
 
 # Install "helm"
-HELM_VERSION=v2.16.7 \
-&& echo -e "\nDownloading Helm Client binary..." \
-&& curl -LO https://get.helm.sh/helm-"$HELM_VERSION"-linux-amd64.tar.gz \
-&& tar xf helm-"$HELM_VERSION"-linux-amd64.tar.gz \
-&& yes | mv ./linux-amd64/helm ./linux-amd64/tiller /usr/local/bin \
-&& rm -rf ./linux-amd64 helm-"$HELM_VERSION"-linux-amd64.tar.gz \
+echo -e "\nDownloading Helm Client binary..." \
+&& curl -LO https://get.helm.sh/helm-v"$HELM_VER"-linux-amd64.tar.gz \
+&& tar xf helm-v"$HELM_VER"-linux-amd64.tar.gz \
+&& yes | mv ./linux-amd64/helm /usr/local/bin \
+&& rm -rf ./linux-amd64 helm-v"$HELM_VER"-linux-amd64.tar.gz \
 && echo -e "\nhelm version:" \
 && helm version 2>/dev/null \
 && source <(helm completion bash)
