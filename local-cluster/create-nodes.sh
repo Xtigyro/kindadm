b4e4cb33c45ec2ca54de7889aeb71d3da29d2826
Xtigyro <miroslav.hadzhiev@gmail.com>
2020-06-11 14:40:49 +0300

    finish helper msgs logic

--- local-cluster/create-nodes.sh
+++ local-cluster/create-nodes.sh
@@ -1,43 +1,48 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -z "$1" ]]; then
+  printf "\nAt least no. of K8s nodes must be set. \nUse \e[32m\e[1m\"bash $0 --help\"\e[00m for details.\n"
+  exit 1
+fi
+
 while [ $# -gt 0 ]; do
   case "$1" in
     --nodes*|-n*)
       if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
       if [[ "$1" != *[1-9][0-9] ]] && [[ "$1" != *[1-9] ]]; then
-        echo -e "\e[32m\n\e[1mNo. of K8s nodes must be: 1-99.\e[00m"
+        printf "\e[32m\n\e[1mNo. of K8s nodes must be: 1-99.\e[00m\n"
         exit 1
       fi
       NO_NODES="${1#*=}"
       ;;
-    --k8s_ver*|-k*)
+    --k8s_ver*|-kv*)
       if [[ "$1" != *=* ]]; then shift; fi
       if [[ "$1" != *.*.* ]]; then
-        echo -e "\e[32m\n\e[1mWrong K8s node version. E.g.: 1.18.2\e[00m"
+        printf "\e[32m\n\e[1mIncompatible K8s node ver.\nCorrect syntax: [number].[number].[number]\e[00m\n"
         exit 2
       fi
       K8S_VER="${1#*=}"
       ;;
-    --helm_ver*|-h*)
+    --helm_ver*|-hv*)
       if [[ "$1" != *=* ]]; then shift; fi
+      if [[ "$1" != *.*.* ]]; then
+        printf "\e[32m\n\e[1mIncompatible Helm ver.\nCorrect syntax: [number].[number].[number]\e[00m\n"
+        exit 2
+      fi
       HELM_VER="${1#*=}"
       ;;
     --help|-h)
-      printf "Meaningful help message" # Flag argument
+      printf "\nUsage:\e[32m\e[1m\n    --helm_ver,-hv      Set Helm version to be deployed.\n    --k8s_ver,-kv       Set K8s version to be deployed.\n    --nodes,-n          Set number of K8s nodes to be created.\n    --help,-h           Prints this message.\n\e[00mExample:\e[32m\e[1m\n    bash create-nodes.sh -n=1 -kv=1.18.2 -hv=3.2.3\e[00m\n" # Flag argument
       exit 0
       ;;
     *)
-      >&2 printf "Error: Invalid argument\n"
-      exit 1
+      >&2 printf "\e[32m\n\e[1mError: Invalid argument\e[00m\n"
+      exit 3
       ;;
   esac
   shift
 done
-
-echo "${NO_NODES}"
-echo "${K8S_VER}"
-exit 0
 
 KIND_CFG="./kind-cfg.yaml"
 KIND_CTRL_CFG=$'\n  - role: control-plane\n    image: kindest/node:'"${K8S_VER}"$'\n    extraMounts:\n      - hostPath: /var/run/docker.sock\n        containerPath: /var/run/docker.sock'
