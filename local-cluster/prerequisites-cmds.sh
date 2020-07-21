9b487d225e1a9d1ccd0cff736a67ab7d2e9cf6dd
Xtigyro <miroslav.hadzhiev@gmail.com>
2020-07-21 14:31:27 +0300

    improve - readme, prereq code style, add demo

--- local-cluster/prerequisites-cmds.sh
+++ local-cluster/prerequisites-cmds.sh
@@ -43,28 +43,28 @@ fi
 
 
 # Unmask and start Docker service
-sudo systemctl unmask docker \
-&& sudo systemctl start docker
+sudo systemctl unmask docker && \
+sudo systemctl start docker
 
 # Install latest "kubectl"
-KUBECTL_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
-echo -e "\nDownloading kubectl binary..." \
-&& curl -LO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl \
-&& chmod +x ./kubectl \
-&& yes | sudo mv ./kubectl /usr/local/bin/kubectl \
-&& echo -e "\nkubectl version:" \
-&& kubectl version --client=true \
-&& source <(kubectl completion bash 2>/dev/null)
+KUBECTL_VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)" && \
+echo -e "\nDownloading kubectl binary..." && \
+curl -LO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl && \
+chmod +x ./kubectl && \
+yes | sudo mv ./kubectl /usr/local/bin/kubectl >/dev/null 2>&1 && \
+echo -e "\nkubectl version:" && \
+kubectl version --client=true && \
+source <(kubectl completion bash 2>/dev/null)
 
 # Install "helm"
-echo -e "\nDownloading Helm Client binary..." \
-&& curl -LO https://get.helm.sh/helm-v"$HELM_VER"-linux-amd64.tar.gz \
-&& tar xf helm-v"$HELM_VER"-linux-amd64.tar.gz \
-&& yes | mv ./linux-amd64/helm /usr/local/bin \
-&& rm -rf ./linux-amd64 helm-v"$HELM_VER"-linux-amd64.tar.gz \
-&& echo -e "\nhelm version:" \
-&& helm version --client=true \
-&& source <(helm completion bash)
+echo -e "\nDownloading Helm Client binary..." && \
+curl -LO https://get.helm.sh/helm-v"$HELM_VER"-linux-amd64.tar.gz && \
+tar xf helm-v"$HELM_VER"-linux-amd64.tar.gz && \
+yes | mv ./linux-amd64/helm /usr/local/bin >/dev/null 2>&1 && \
+rm -rf ./linux-amd64 helm-v"$HELM_VER"-linux-amd64.tar.gz && \
+echo -e "\nhelm version:" && \
+helm version --client=true && \
+source <(helm completion bash)
 
 # Install/update Helm plugins: "helm-diff", "tiller"
 echo -e "\nInstalling/updating Helm plugins: \"helm-diff\" and \"tiller\"..."
@@ -76,18 +76,19 @@ echo -e "\nInstalled Helm plugins:"
 helm plugin list 2>/dev/null
 
 # Install latest "helmfile"
-echo -e "\nDownloading Helmfile binary..." \
-&& curl -LO https://github.com/roboll/helmfile/releases/latest/download/helmfile_linux_amd64 \
-&& chmod +x ./helmfile_linux_amd64 \
-&& yes | mv ./helmfile_linux_amd64 /usr/local/bin/helmfile \
-&& helmfile -v
+echo -e "\nDownloading Helmfile binary..." && \
+curl -LO https://github.com/roboll/helmfile/releases/latest/download/helmfile_linux_amd64 && \
+chmod +x ./helmfile_linux_amd64 && \
+yes | mv ./helmfile_linux_amd64 /usr/local/bin/helmfile >/dev/null 2>&1 && \
+echo -e "\n" && \
+helmfile -v
 
 # Install kINd
-KIND_VERSION=v0.8.1 \
-&& echo -e "\nDownloading kINd binary..." \
-&& curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/"$KIND_VERSION"/kind-$(uname)-amd64 \
-&& chmod +x ./kind \
-&& yes | mv ./kind /usr/local/bin/kind \
-&& echo -e "\nkINd version:" \
-&& kind version \
-&& source <(kind completion bash)
+KIND_VERSION=v0.8.1 && \
+echo -e "\nDownloading kINd binary..." && \
+curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/"$KIND_VERSION"/kind-$(uname)-amd64 && \
+chmod +x ./kind && \
+yes | mv ./kind /usr/local/bin/kind >/dev/null 2>&1 && \
+echo -e "\nkINd version:" && \
+kind version && \
+source <(kind completion bash)
