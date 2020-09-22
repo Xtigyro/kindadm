bf0b81263216b7350192cbe0f58ded413e29b4a3
Xtigyro <miroslav.hadzhiev@gmail.com>
2020-09-23 00:47:50 +0300

    rebrand. organize helmfiles by app. optional apps. add weavescope.

--- local-cluster/create-cluster.sh
+++ local-cluster/create-cluster.sh
@@ -4,8 +4,9 @@ set -e
 LIGHT_GREEN='\033[1;32m'
 LIGHT_RED='\033[1;31m'
 NC='\033[0m'   # No Color
-KIND_CFG="$(<./kind-base-config.yaml)"   # base config file
+KIND_CFG="$(<./templates/kind-base-config.yaml)"   # base config file
 K8S_CLUSTERS="$(kind get clusters 2>/dev/null | tr '\n' ' ' | sed 's/[[:blank:]]*$//')"
+SUPPORTED_OPT_APPS="$(ls -d helmfiles/apps/optional/*/ | cut -f4 -d'/')"
 
 if [[ -z "$1" ]]; then
   printf "\nAt least no. of K8s nodes must be set. \nUse ${LIGHT_GREEN}\"bash $0 --help\"${NC} for details.\n"
@@ -25,6 +26,21 @@ function contains_string {
   return "$result"
 }
 
+function contain_strings_from_strings {
+  local list_a="$1"
+  local list_b="$2"
+  for str in "$list_a"; do
+    if [[ "$list_b" =~ (^|,)"$str"(,|$) ]]; then
+      # yes, list includes item
+      result=0
+    else
+      result=1
+      break
+    fi
+  done
+  return "$result"
+}
+
 function purge_clusters {
   select choice in "ALL_CLUSTERS" "PER_CLUSTER"; do
   case "$choice" in
@@ -36,12 +52,12 @@ function purge_clusters {
     read -p "[ ${K8S_CLUSTERS} ]: " K8S_CLUSTER;
     if ! `contains_string "${K8S_CLUSTERS}" "$K8S_CLUSTER"`; then
       echo "Invalid cluster name."
-      exit 2
+      exit 3
     fi
     declare -g clusters=("$K8S_CLUSTER");
     break;;
     *) echo "'ALL_CLUSTERS' or 'PER_CLUSTER' must be chosen.";
-    exit 3;
+    exit 4;
     break;;
   esac
   done
@@ -52,7 +68,7 @@ while [ $# -gt 0 ]; do
     --nodes=*|-n=*)
       if [[ "$1" != *=[1-9] ]] && [[ "$1" != *=[1-9][1-9] ]]; then
         printf "\nNo. of K8s nodes must be: ${LIGHT_GREEN}1-99${NC}.\n"
-        exit 4
+        exit 5
       fi
       NO_NODES="${1#*=}"
       ;;
@@ -72,30 +88,55 @@ while [ $# -gt 0 ]; do
       if [[ "$1" == *=* ]]; then NODE_TAINT_LABEL="${1#*=}"; fi
       COEFFICIENT_TAINT=0.5
       ;;
+    --opt-apps=*|-oa=*)
+      if [[ "$1" == *=all ]]; then
+        OPT_APPS=true
+      else
+        OPT_APPS="${1#*=}"
+        if ! `contain_strings_from_strings "$SUPPORTED_OPT_APPS" "$OPT_APPS"`; then
+          printf "\nSupported optional apps (comma-separated): ${LIGHT_GREEN}"$SUPPORTED_OPT_APPS"${NC}.\n"
+          exit 6
+        fi
+      fi
+      ;;
     --k8s_ver=*|-v=*)
       if [[ "$1" != *=1.*.* ]]; then
         printf "\nIncompatible K8s node ver.\nCorrect syntax/version: ${LIGHT_GREEN}1.[x].[x]${NC}\n"
-        exit 5
+        exit 7
       fi
       K8S_VER="${1#*=}"
       ;;
     --purge|-p)
       purge_clusters
-      for ((i=0;i<"${#clusters[@]}";i++));
-        do
+      for ((i=0;i<"${#clusters[@]}";i++)); do
           kind delete cluster --name "${clusters[i]}"
-        done
+      done
       printf "\n${LIGHT_GREEN}Clusters left:${NC}\n"
       kind get clusters
       exit 0
       ;;
+    --list-oa|-loa)
+        printf "\nList of supported optional apps:\
+        \n    ${LIGHT_GREEN}$SUPPORTED_OPT_APPS${NC}\n"
+      exit 0
+      ;;
     --help|-h)
-      printf "\nUsage:\n    ${LIGHT_GREEN}--k8s_ver,-v${NC}         Set K8s version to be deployed.\n    ${LIGHT_GREEN}--nodes,-n${NC}           Set number of K8s nodes to be created.\n    ${LIGHT_GREEN}--all-labelled,-al${NC}   Set labels on all K8s nodes.\n    ${LIGHT_GREEN}--half-labelled,-hl${NC}  Set labels on half K8s nodes.\n    ${LIGHT_GREEN}--all-tainted,-at${NC}    Set taints on all K8s nodes. A different label can be defined.\n    ${LIGHT_GREEN}--half-tainted,-ht${NC}   Set taints on half K8s nodes. A different label can be defined.\n    ${LIGHT_GREEN}--purge,-p${NC}           Purges interactively any existing clusters and temp configs.\n    ${LIGHT_GREEN}--help,-h${NC}            Prints this message.\nExample:\n    ${LIGHT_GREEN}bash $0 -n=2 -v=1.18.2 -hl='nodeType=devops' -ht ${NC}\n" # Flag argument
+      printf "\nUsage:\n    ${LIGHT_GREEN}--k8s_ver,-v${NC}         Set K8s version to be deployed.\
+        \n    ${LIGHT_GREEN}--nodes,-n${NC}           Set number of K8s nodes to be created.\
+        \n    ${LIGHT_GREEN}--all-labelled,-al${NC}   Set labels on all K8s nodes.\
+        \n    ${LIGHT_GREEN}--half-labelled,-hl${NC}  Set labels on half K8s nodes.\
+        \n    ${LIGHT_GREEN}--all-tainted,-at${NC}    Set taints on all K8s nodes. A different label can be defined.\
+        \n    ${LIGHT_GREEN}--half-tainted,-ht${NC}   Set taints on half K8s nodes. A different label can be defined.\
+        \n    ${LIGHT_GREEN}--purge,-p${NC}           Purges interactively any existing clusters and temp configs.\
+        \n    ${LIGHT_GREEN}--opt-apps,-oa${NC}       Deploy supported optional app(s).\
+        \n    ${LIGHT_GREEN}--list-oa,-loa${NC}       List supported optional app(s).\
+        \n    ${LIGHT_GREEN}--help,-h${NC}            Prints this message.\
+        \nExample:\n    ${LIGHT_GREEN}bash $0 -n=2 -v=1.19.1 -hl='nodeType=devops' -ht -oa=weave-scope${NC}\n" # Flag argument
       exit 0
       ;;
     *)
       >&2 printf "\nError: ${LIGHT_GREEN}Invalid argument${NC}\n"
-      exit 6
+      exit 8
       ;;
   esac
   shift
@@ -133,11 +174,14 @@ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admi
 # Init Helm
 helm init --service-account tiller
 
-# Deploy desired svc-s
-helmfile -f ./helmfile.yaml apply > /dev/null
+# Deploy default apps
+helmfile -f ./helmfiles/apps/default/helmfile.yaml apply > /dev/null
+
+# Deploy optional apps
+helmfile -f ./helmfiles/apps/optional/helmfile.yaml apply > /dev/null
 
 # Deploy Kubernetes Dashboard Admin ClusterRoleBinding
-kubectl apply -f ./k8s-dashboard-rolebinding.yaml
+kubectl apply -f ./templates/k8s-dashboard-rolebinding.yaml
 
 # Get node names
 CLUSTER_WRKS=$(kubectl get nodes | tail -n +2 | cut -d' ' -f1)
