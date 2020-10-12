b2c44a7d46cc0d9eb388bf1ad2678c7c000838ca
Xtigyro <miroslav.hadzhiev@gmail.com>
2020-10-12 11:40:50 +0000

    add bash completion for all binaries

--- local-cluster/setup.sh
+++ local-cluster/setup.sh
@@ -112,6 +112,7 @@ if ! `"$EXEC_DIR"/helm version --client=true 2>/dev/null | grep -q "$HELM_VER"` 
 else
   echo -e "\nHelm present:" && \
   "$EXEC_DIR"/helm version --client=true
+  source <("$EXEC_DIR"/helm completion bash 2>/dev/null)
 fi
 
 # Install/update Helm plugins: "helm-diff"
@@ -166,4 +167,5 @@ if ! `"$EXEC_DIR"/kind version 2>/dev/null | grep -q "$KIND_VERSION"` ; then
 else
   echo -e "\nPresent:" && \
   "$EXEC_DIR"/kind version
+  source <("$EXEC_DIR"/kind completion bash 2>/dev/null)
 fi
