28aef641b2109fb27aa63f1aa1121fb219b78579
Xtigyro <miroslav.hadzhiev@gmail.com>
2021-05-13 17:12:35 +0300

    update components versions

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,10 +2,10 @@
 set -eu
 
 # default versions
-HELMFILE_VER='0.138.7'
+HELMFILE_VER='0.139.4'
 HELM_PLUGIN_DIFF_VER='3.1.3'
 KIND_VERSION='0.10.0'
-KUBECTL_VERSION='1.20.5'
+KUBECTL_VERSION='1.20.6'
 
 # passed on vars
 HELM_VER="$1"
